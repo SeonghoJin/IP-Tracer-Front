f8de0744a90adf6467e39fd4f9690ff6c474225e
moretall <seongho4996@naver.com>
2021-11-23 14:26:21 +0900

    feat : app config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
+export type AppConfig = {
+  port: string;
+};
+
 export const appConfig = registerAs('app', () => ({
   port: process.env.PORT,
 }));
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
+import { ConfigService } from '@nestjs/config';
+import { AppConfig } from './config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const appConfig = app.get();
+  const appConfig = app.get(ConfigService).get('app') as AppConfig;
   app.useWebSocketAdapter(new WsAdapter(app));
-
-  await app.listen(8080);
+  await app.listen(appConfig.port);
 }
 
 bootstrap();
