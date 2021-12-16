c9350022f1c3eb5ee31c512ab351f047984385be
moretall <seongho4996@naver.com>
2021-12-16 16:25:02 +0900

    cors 수정

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ConfigService } from '@nestjs/config';
 import { AppConfig } from './config/app.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   const appConfig = app.get(ConfigService).get('app') as AppConfig;
   app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(appConfig.port);
