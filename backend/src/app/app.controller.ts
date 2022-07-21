ccaf5bf20e51ff606e3c6b70f4f07d67918c3bda
moretall <seongho4996@naver.com>
2022-07-21 23:37:56 +0900

    fix(api): api 이름 수정

--- backend/src/app/app.controller.ts
+++ backend/src/app/app.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
--- backend/src/ip-lookup/ip-lookup.controller.ts
+++ backend/src/ip-lookup/ip-lookup.controller.ts
@@ -6,23 +6,22 @@ import {
 import { IpLookupService } from './ip-lookup.service';
 import { IpLocationResponseDto } from './dto/ip-location.response.dto';
 
-@Controller('ip-lookup')
+@Controller('api/ip-lookup')
 export class IpLookupController {
   constructor(private readonly ipLookupService: IpLookupService) {}
 
   @Get('/location/:ip')
   async findLocation(@Param('ip') ip): Promise<IpLocationResponseDto> {
-    console.log(ip);
     return await this.ipLookupService.getLocation(ip);
   }
 
-  @Get('/api/health')
+  @Get('/health')
   async getApiHealths() {
     const apiStatus = await this.ipLookupService.getApiHealths();
     return apiStatus;
   }
 
-  @Get('/api/health/:apiName')
+  @Get('/health/:apiName')
   async getApiHealth(@Param('apiName') apiName: string) {
     const apiStatus = await this.ipLookupService.getApiHealth(apiName);
     return apiStatus;
